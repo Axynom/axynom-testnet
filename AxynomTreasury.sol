b7ed354eb930b882d6ee6cb5771472b1cecbba3d
gruv | Axynom <pogbitnetwork@gmail.com>
2025-05-29 18:35:03 +0300

    Update AxynomTreasury.sol

--- AxynomTreasury.sol
+++ AxynomTreasury.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
