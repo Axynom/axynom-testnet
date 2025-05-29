517b620c4eb16a0679d08321a37067969bc3e57c
gruv | Axynom <pogbitnetwork@gmail.com>
2025-05-29 18:36:10 +0300

    Update StakeLogic.sol

--- libraries/StakeLogic.sol
+++ libraries/StakeLogic.sol
@@ -4,6 +4,14 @@ pragma solidity ^0.8.25;
 library StakeLogic {
     uint256 internal constant SECONDS_IN_YEAR = 365 days;
 
+    struct StakeInfo {
+        uint256 amount;
+        uint256 startTimestamp;
+        uint256 lockDuration;
+        uint256 apy;
+        bool claimed;
+    }
+
     function calculateReward(
         uint256 amount,
         uint256 apy,
@@ -12,12 +20,17 @@ library StakeLogic {
         reward = (amount * apy * duration) / (100 * SECONDS_IN_YEAR);
     }
 
-    /// @notice Applies a 10% bonus if stake is being re-staked after maturity
-    function applyReStakeBonus(uint256 baseReward) internal pure returns (uint256) {
-        return baseReward + (baseReward * 10) / 100;
+    function getAvailableReward(
+        StakeInfo memory s
+    ) internal view returns (uint256) {
+        if (s.claimed) return 0;
+        uint256 elapsed = block.timestamp - s.startTimestamp;
+        return calculateReward(s.amount, s.apy, elapsed);
     }
 
-    function hasCompletedStake(uint256 startTimestamp, uint256 duration) internal view returns (bool) {
-        return block.timestamp >= startTimestamp + duration;
+    function isMature(
+        StakeInfo memory s
+    ) internal view returns (bool) {
+        return !s.claimed && block.timestamp >= s.startTimestamp + s.lockDuration;
     }
 }
